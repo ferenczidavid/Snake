91c8db194995871498d25655ea301b06d57523f4
Ferenczi Dávid <v0xtzf@inf.elte.hu>
2021-10-28 11:20:04 +0200

    Window frame for snake game

--- src/Snake/Main.java
+++ src/Snake/Main.java
@@ -1,9 +1,23 @@
 package Snake;
 
-public class Main {
+import javax.swing.*;
 
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Main {
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("2103");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new Snake("Snake");
+                frame.setSize(510, 510);
+                frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                frame.setResizable(false);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
     }
 }
